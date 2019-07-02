f604edba1bed811c10294ecd9ac39ad70c1dc71c
aaronbrown1989 <45995788+aaronbrown1989@users.noreply.github.com>
2019-07-02 11:49:28 +0100

    Added Comic Books view

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -6,17 +6,11 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicsBookController : Controller
+    public class ComicBooksController : Controller
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic book controller!");
-
+            return View();
         }
     }
 }
